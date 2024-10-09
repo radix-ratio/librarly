b1da5ca509cb8ef5ef2eaadd15b3c23390cc3950
Kacper <[k.kacper.korzeniewski@gmail.com]>
2024-10-09 21:49:03 +0200

    add data to myLibrary array

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 'use strict';
     
-const myLibrary = [];
+const myLibrary = [
+    {
+        title: 'The Hobbit',
+        author: 'J.R.R. Tolkien',
+        pages: 295
+    },
+    {
+        title: 'Harry Potter',
+        author: 'J.K. Rowling',
+        pages: 400
+    },  
+    {
+        title: 'Lord of the Rings',
+        author: 'J.R.R. Tolkien',
+        pages: 700
+    }
+];
 
 function Book(title, author, pages) {
     this.title = title;
